9bdc65a52572a1dc51788f5f0e68b6345d50e19d
keef hassan <keefhassan4@gmail.comgit config --global user.email keefhassan4@gmail.coome>
2024-03-28 10:29:12 -0500

    create pat tables Co-authored-by: Beth Tracy <bntracy@gmail.com> Co-authored-by: yalvarez08 <yalvarez08@users.noreply.github.com>

--- Models/PetOwner.cs
+++ Models/PetOwner.cs
@@ -5,5 +5,23 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 namespace pet_hotel
 {
-    public class PetOwner {}
+    public class PetOwner
+    {
+
+
+        public int id { get; set; }
+        [Required]
+        public string name { get; set; }
+        [Required]
+
+        public string emailAddress { get; set; }
+
+        //  public int { get; set; }
+
+
+
+
+    }
+
+
 }
